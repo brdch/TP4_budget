9de69d8570e9472f867c720a360dab8d0eb2ace5
Manuel Ramirez <manuel-etudes@outlook.com>
2022-11-11 15:55:50 -0500

    fetch-depth 0

--- myscript.py
+++ myscript.py
@@ -1,4 +1,5 @@
 import os
+
 os.system('git bisect start c1a4be04b972b6c17db242fc37752ad517c29402 e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c')
 os.system('git bisect good 33827ba712f81ebc7c1fc935e8115e9a49065a9a')
 os.system('git bisect good bfdccab909c32635457d41eeb6e7fed322026170')
